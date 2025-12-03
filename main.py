39210389202a575e0ff13a603371374ce837fd95
al1enn <wipetmp1404@proton.me>
2025-12-03 18:51:16 +0500

    add handler for any medi

--- main.py
+++ main.py
@@ -3,16 +3,32 @@ from aiogram import types
 from aiogram.filters import CommandStart, Command
 from config import token
 import asyncio
+from aiogram.types import FSInputFile
 
 my_bot = Bot(token=str(token))
 dp = Dispatcher()
 
+any_media = F.photo | F.document | F.video
 
-@dp.message(F.photo)
-async def handle_any_photo_message(message: types.Message):
-    await message.copy_to(
-        chat_id=message.chat.id,
-    )
+
+@dp.message(any_media)
+async def handle_any_media_message(message: types.Message):
+    if message.photo:
+        await message.answer_photo(
+            photo=message.photo[1].file_id,
+        )
+    if message.document:
+        await message.answer_document(
+            document=message.document.file_id,
+        )
+    if message.video:
+        await message.answer_video(
+            video=message.video.file_id,
+        )
+
+    # await message.copy_to(
+    #     chat_id=message.chat.id,
+    # )
 
 
 @dp.message(CommandStart())
@@ -27,6 +43,14 @@ async def handle_start(message: types.Message):
 async def hanle_info(message: types.Message):
     await message.answer(
         text="Это тестовый бот для изучения aiogram",
+    )
+
+
+@dp.message(Command("photo"))
+async def photo_cmd(message: types.Message):
+    photo = FSInputFile("/home/alien/Pictures/icon - edid this.png")
+    await message.answer_photo(
+        photo=photo,
     )
 
 
