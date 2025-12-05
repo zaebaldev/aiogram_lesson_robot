9ba2f0e20a85574852d9d89913f161d07a57e384
al1enn <wipetmp1404@proton.me>
2025-12-05 19:24:06 +0500

    create keyboards package

---
+++ keyboards/__init__.py
@@ -0,0 +1,1 @@
+from .info_kb import create_info_kb_builder, create_info_kb_markup
--- routers/commands/info_cmd.py
+++ routers/commands/info_cmd.py
@@ -2,9 +2,7 @@ from aiogram import Router, F
 from aiogram import types
 from aiogram.filters import CommandStart, Command
 from config import admin_ids
-from aiogram.types.reply_keyboard_markup import ReplyKeyboardMarkup
-from aiogram.types.keyboard_button import KeyboardButton
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from keyboards import create_info_kb_builder, create_info_kb_markup
 
 router = Router()
 
@@ -12,26 +10,10 @@ router = Router()
 @router.message(CommandStart())
 async def handle_start(message: types.Message):
     print(message.from_user.id)
-    # btn1 = KeyboardButton(text="test")
-    # btn2 = KeyboardButton(text="some text")
-    # btn3 = KeyboardButton(text="something")
-    # keyboard = ReplyKeyboardMarkup(
-    #     keyboard=[
-    #         [btn1, btn2],
-    #         [btn3],
-    #     ],
-    #     resize_keyboard=True,
-    # )
-    builder = ReplyKeyboardBuilder()
-    for i in range(1, 11):  # 1,2,3,4,5,6,7,8,9,10
-        builder.button(
-            text=f"Set {i}",
-        )
-    # builder.add(btn1, btn2, btn3)
-    builder.adjust(3)
+
     await message.answer(
         text=f"Добро  пожаловать, {message.from_user.full_name}",
-        reply_markup=builder.as_markup(),
+        reply_markup=create_info_kb_markup(),
     )
 
 
