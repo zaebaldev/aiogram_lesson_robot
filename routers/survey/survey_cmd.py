802e4728dc2e26cadbfc8298cc7ed0a36175eb5d
al1enn <wipetmp1404@proton.me>
2025-12-06 20:04:00 +0500

    validate name

--- routers/survey/survey_cmd.py
+++ routers/survey/survey_cmd.py
@@ -1,4 +1,4 @@
-from aiogram import Router, F
+from aiogram import Router
 from aiogram import types
 from aiogram.filters import Command
 from aiogram.fsm.state import State, StatesGroup
@@ -29,7 +29,23 @@ async def handle_survey_name_state(
     message: types.Message,
     state: FSMContext,
 ):
-    await message.answer(
-        text=f"Your name is {message.text}",
-    )
-    await state.set_state(Survey.surname)
+    text = message.text
+    if text.isalpha():
+        await state.update_data(name=text)
+        await message.answer(
+            text="What is your surname?",
+        )
+        await state.set_state(Survey.surname)
+    else:
+        await message.answer(
+            text="Please, enter correct name",
+        )
+
+
+@router.message(Survey.surname)
+async def handle_survey_surname_state(
+    message: types.Message,
+    state: FSMContext,
+):
+    await state.update_data(surname=message.text)
+    await state.set_state(Survey.age)
